f2c2806425496353b1228e15fa137dd853acc3a6
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-09 21:38:04 +0300

    Updating Customer class in by adding relation between it and Cart

--- src/main/java/com/mealmate/entity/Customer.java
+++ src/main/java/com/mealmate/entity/Customer.java
@@ -16,21 +16,17 @@ public class Customer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "total_price")
-    private Double totalPrice;
 //    @OneToOne(cascade = CascadeType.ALL)     //it will be added after implementing security
 //    private User user
-    @OneToOne(cascade = CascadeType.ALL)
-    private Cart cart;
+
 //    @OneToOne(cascade = CascadeType.ALL)/  /it will be added after implementing order class
 //    private List<Order> orders;
     @OneToMany(cascade = CascadeType.ALL)
     private List<Address> addresses;
 
+    @OneToOne(mappedBy = "customer",cascade = CascadeType.ALL)
+    private Cart cart ;
 
-    @Enumerated(EnumType.STRING)
-    private Integer status;
 
-    private double discount;
 
 }

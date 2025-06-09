beb4050428a0897d5e4709bff5165b61c4f19a43
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-09 17:31:22 +0300

    implement CartItem class in entity package

---
+++ src/main/java/com/mealmate/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.mealmate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart_item")
+@EqualsAndHashCode(callSuper = false)
+public class CartItem extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    @Column(name = "unit_price")  //we need to update it to call its value from menu
+    private Integer unitPrice;
+    @Column(name = "total_price",insertable=false , updatable=false)
+    private double totalPrice;
+
+
+
+
+
+
+}

2dfadeb63c67704349baf29420e7c32e5df975e9
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-09 17:17:02 +0300

    implement cart class in entity package

---
+++ src/main/java/com/mealmate/entity/Cart.java
@@ -0,0 +1,29 @@
+package com.mealmate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart")
+@EqualsAndHashCode(callSuper = false)
+public class Cart extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "total_price")
+    private Double totalPrice;
+    @Column(name = "total_items")   // we need to update it to make it driven
+    private Integer totalItems;
+
+    @Enumerated(EnumType.STRING)
+    private Integer status;
+
+    private double discount;
+
+}

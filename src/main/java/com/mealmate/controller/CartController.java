bbad7d362a26fdea0f53830e83589b88b73b2678
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 20:28:12 +0300

    updating CartController by implementing post method addItemToCart

--- src/main/java/com/mealmate/controller/CartController.java
+++ src/main/java/com/mealmate/controller/CartController.java
@@ -1,8 +1,12 @@
 package com.mealmate.controller;
 
-import com.mealmate.repository.CartRepository;
+import com.mealmate.request.CartItemRequest;
+import com.mealmate.response.CartResponse
 import com.mealmate.services.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CartController {
 private final CartService cartService;
+
+@PostMapping("/add/cartItem")
+ResponseEntity<CartResponse> addCartItem(@RequestBody CartItemRequest request) {
+
+
+}
 
 
 

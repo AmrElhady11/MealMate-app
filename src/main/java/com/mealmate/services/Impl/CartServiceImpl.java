d31a7e6f0517dd4f5d83de23138342b5b39df7d2
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 20:54:03 +0300

    updating code in CartServiceImpl

--- src/main/java/com/mealmate/services/Impl/CartServiceImpl.java
+++ src/main/java/com/mealmate/services/Impl/CartServiceImpl.java
@@ -1,9 +1,13 @@
 package com.mealmate.services.Impl;
 
 import com.mealmate.entity.Cart;
+import com.mealmate.entity.CartItem;
+import com.mealmate.exception.NotFoundException;
+import com.mealmate.repository.CartItemRepository;
 import com.mealmate.repository.CartRepository;
 import com.mealmate.request.CartItemRequest;
 import com.mealmate.services.CartService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +15,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
     @Override
+    @Transactional
     public Cart addItemToCart(CartItemRequest cartItemRequest) {
-        var cart = cartRepository.findById(cartItemRequest.getCartId()).orElseThrow()
+        var cart = cartRepository.findById(cartItemRequest.getCartId()).orElseThrow(()-> new NotFoundException(String.format("The cart with id : %d is not exist", cartItemRequest.getCartId())));
+        CartItem cartItem = CartItem.builder()
+                .cart(cart)
+                .unitPrice(cartItemRequest.getUnitPrice())
+                .id(cartItemRequest.getCartId())
+                .quantity(cartItemRequest.getQuantity())
+                .build();
+        // we will calculate the total price for item
+        // save cart item
+
         return null;
     }
 

a384c0ee441ef313b91470384ec8fdaac13743de
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 19:54:18 +0300

    implement HandlerException as global Exception Handler

---
+++ src/main/java/com/mealmate/exception/HandlerException.java
@@ -0,0 +1,22 @@
+package com.mealmate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.mealmate.response.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandlerException {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+          ErrorResponse error = ErrorResponse.builder().
+                  message(e.getMessage())
+                  .code(HttpStatus.NOT_FOUND.value())
+                  .timestamp(System.currentTimeMillis())
+                  .build();
+          return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+}

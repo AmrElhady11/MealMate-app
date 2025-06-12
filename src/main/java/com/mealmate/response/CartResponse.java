c3cac261764e95b3325bb1cb8b19c8b24480f48e
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 20:27:07 +0300

    Creating CartResponse

---
+++ src/main/java/com/mealmate/response/CartResponse.java
@@ -0,0 +1,23 @@
+package com.mealmate.response;
+
+import com.mealmate.entity.CartItem;
+import com.mealmate.enums.CartStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartResponse {
+    private Integer cartId;
+    private Double totalPrice;
+    private List<CartItemResponse> cartItems;
+    @Builder.Default
+    private CartStatus status = CartStatus.READ_WRITE;
+
+}

77a09d015319526c103b85553b0de36271d3fcf8
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-12 19:53:36 +0300

    Refactoring and updating ErrorResponse;

--- src/main/java/com/mealmate/response/ErrorResponse.java
+++ src/main/java/com/mealmate/response/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.mealmate.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +10,9 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ErrorResponse {
     private String message;
     private int code;
-    private LocalDateTime timestamp;
+    private Long timestamp;
 }

919993e019924288ad01e0ee33350a0f4e24408a
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 23:21:11 +0300

    Creating MenuService class

--- src/main/java/com/mealmate/services/MenuService.java
+++ src/main/java/com/mealmate/services/MenuService.java
@@ -1,4 +1,22 @@
 package com.mealmate.services;
 
+import com.mealmate.entity.Menu;
+import com.mealmate.entity.MenuItem;
+import com.mealmate.request.MenuItemRequest;
+import com.mealmate.request.MenuRequest;
+
+import java.util.List;
+
 public interface MenuService {
+
+    Menu addMenu(MenuRequest menuRequest);
+    MenuItem addMenuItem(MenuItemRequest menuItemRequest);
+    MenuItem updateMenuItem(MenuItemRequest menuItemRequest);
+    void deleteMenu(Long MenuId);
+    void clearMenu(Long manuId);
+    void deleteMenuItem(Long menuItemId);
+    List<MenuItem> browseMenu(Long MenuId);
+    List<Menu> browseAllRestaurantMenus(Long restaurantId);
+    List<MenuItem> getMenuItemByTitle(String title);
+
 }

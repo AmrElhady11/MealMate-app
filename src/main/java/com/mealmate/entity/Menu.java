d7827895a8b37e385ae5d51273859a44213e0c08
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 23:16:47 +0300

    refactoring Menu by adding relation to Restaurant and implement MenuBuilderMethod

--- src/main/java/com/mealmate/entity/Menu.java
+++ src/main/java/com/mealmate/entity/Menu.java
@@ -1,10 +1,11 @@
 package com.mealmate.entity;
 
+import com.mealmate.request.MenuRequest;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
-
+@Builder
 @Setter
 @Getter
 @NoArgsConstructor
@@ -19,10 +20,20 @@ public class Menu extends BaseEntity {
     private String name;
     private String description;
 
-//    @ManyToOne
-//    @JoinColumn(name = "restaurant_id")
-//    private Restaurant restaurant;
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
 
+public static Menu buildMenu(MenuRequest request){
+    Menu menu = Menu.builder()
+                .id(request.getId())
+                .name(request.getName())
+                .description(request.getDescription())
+                .build();
+    menu.setCreatedBy(request.getCreatedBy());
+    menu.setUpdatedBy(request.getUpdatedBy());
+    return menu;
 
+}
 
 }

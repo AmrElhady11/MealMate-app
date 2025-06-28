775b4122ec83e1cc41ce95f6870120142b88ec49
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 16:22:32 +0300

    Editing and adding some methods

--- src/main/java/com/mealmate/services/CartService.java
+++ src/main/java/com/mealmate/services/CartService.java
@@ -1,8 +1,22 @@
 package com.mealmate.services;
 
 import com.mealmate.entity.Cart;
+import com.mealmate.entity.CartItem;
+import com.mealmate.entity.MenuItem;
+import com.mealmate.enums.CartStatus;
 import com.mealmate.request.CartItemRequest;
+import com.mealmate.request.CartRequest;
+
+import java.util.List;
 
 public interface CartService {
     Cart addItemToCart(CartItemRequest cartItemRequest);
+    void removeItemFromCart(Long cartItemId);
+    void clearCart(Long cartId);
+    CartItem updateCartItem(CartItemRequest cartItemRequest);
+    List<CartItem> browseCart(Long cartId);
+    Cart getCartStatus(Long cartId);
+    Cart updateCartStatus(Long cartId, CartStatus newCartStatus);
+    MenuItem checkItemAvailability(Long itemId);
+    Cart createCart(CartRequest cartRequest);
 }

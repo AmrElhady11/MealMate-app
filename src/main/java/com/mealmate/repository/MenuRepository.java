eea19f163cb060206b5487e1a547d91fc4155739
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 23:20:22 +0300

    creating findAllByRestaurantId method

--- src/main/java/com/mealmate/repository/MenuRepository.java
+++ src/main/java/com/mealmate/repository/MenuRepository.java
@@ -4,5 +4,8 @@ import com.mealmate.entity.CartItem;
 import com.mealmate.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    List<Menu> findAllByRestaurantId(Long restaurantId);
 }

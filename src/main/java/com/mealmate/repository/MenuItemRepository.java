4ad5638c4981afb66b26d20fb274b7107eee12ab
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 23:19:12 +0300

    creating two methods (delete by menuId,findMenuItemByMenuId)

--- src/main/java/com/mealmate/repository/MenuItemRepository.java
+++ src/main/java/com/mealmate/repository/MenuItemRepository.java
@@ -3,5 +3,9 @@ package com.mealmate.repository;
 import com.mealmate.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+    void deleteMenuItemByMenuId(Long menuId);
+    List<MenuItem> findMenuItemByMenuId(Long menuId);
 }

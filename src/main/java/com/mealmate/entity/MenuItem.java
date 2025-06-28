4733954f414169b67fa79bc6872ea1d929180e74
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 23:18:06 +0300

    implement MenuItemBuilder method

--- src/main/java/com/mealmate/entity/MenuItem.java
+++ src/main/java/com/mealmate/entity/MenuItem.java
@@ -1,8 +1,9 @@
 package com.mealmate.entity;
 
+import com.mealmate.request.MenuItemRequest;
 import jakarta.persistence.*;
 import lombok.*;
-
+@Builder
 @Setter
 @Getter
 @NoArgsConstructor
@@ -17,9 +18,21 @@ public class MenuItem extends BaseEntity {
     private String title;
     private String description;
     private Double price;
+    private Integer quantity;
 
     @ManyToOne
     @JoinColumn(name = "menu_id")
     private Menu menu;
 
+    public static MenuItem itemBuilder(MenuItemRequest menuRequest) {
+        MenuItem menuItem = MenuItem.builder()
+                .description(menuRequest.getDescription())
+                .title(menuRequest.getTitle())
+                .price(menuRequest.getPrice())
+                .quantity(menuRequest.getQuantity())
+                .build();
+        menuItem.setCreatedBy(menuItem.getCreatedBy());
+        menuItem.setUpdatedBy(menuItem.getUpdatedBy());
+        return menuItem;
+    }
 }

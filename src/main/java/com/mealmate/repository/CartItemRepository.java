afbaf049f1906b3d5c2f7916f74841eef5000667
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-28 16:21:48 +0300

    adding methods in the repo

--- src/main/java/com/mealmate/repository/CartItemRepository.java
+++ src/main/java/com/mealmate/repository/CartItemRepository.java
@@ -3,5 +3,9 @@ package com.mealmate.repository;
 import com.mealmate.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteByCartId(Long cartId);
+    List<CartItem> findAllByCartId(Long cartId);
 }

1c2ab5173c2e4c0cb0281b2af36e65020168a4da
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-06-29 19:55:12 +0300

    adding new custom method to RestaurantRepository

--- src/main/java/com/mealmate/repository/RestaurantRepository.java
+++ src/main/java/com/mealmate/repository/RestaurantRepository.java
@@ -1,7 +1,11 @@
 package com.mealmate.repository;
 
 import com.mealmate.entity.Restaurant;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Page<Restaurant> findAll( Pageable pageable);
 }
